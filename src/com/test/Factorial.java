9cd9fbd658ce8d66ab9ab772b1f34aa433cb86f4
hanwen <1106002609@qq.com>
2016-03-14 21:49:50 +0800

    阶乘简单的实现

--- src/com/test/Factorial.java
+++ src/com/test/Factorial.java
@@ -3,7 +3,7 @@ package com.test;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-public class JieCheng {
+public class Factorial {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		long n = sc.nextLong();
