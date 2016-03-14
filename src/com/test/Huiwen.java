9c3a79c49c64a6974a5d5c1fa3b4790e69a75ebf
hanwen <1106002609@qq.com>
2016-03-14 21:48:45 +0800

    回文数

--- src/com/test/Huiwen.java
+++ src/com/test/Huiwen.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Huiwen {
 	
 	public static void main(String[] args) {
-		
+	  	
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		for(int a = 1; a < 10; a++) {
