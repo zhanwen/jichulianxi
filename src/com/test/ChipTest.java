3007de69162fc97c9d808e652bda11a01c62d2ba
hanwen <1106002609@qq.com>
2016-03-16 21:40:34 +0800

    芯片测试

---
+++ src/com/test/ChipTest.java
@@ -0,0 +1,33 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class ChipTest {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int[][] a = new int[n][n];
+		
+		for(int i = 0; i < a.length; i++) {
+			for(int j = 0; j < a[i].length; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
+		for(int j = 0; j < n; j++) {
+			int count = 0;
+			
+			for(int i = 0; i <n; i++) {
+				count += a[j][i];
+			}
+			
+			if(count > n/2) {
+				System.out.print((int)(j+1) + " ");
+			}
+			
+		}
+	}
+	
+}
