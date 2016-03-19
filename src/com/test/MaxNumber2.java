0d1a0ee1a16962459b8aeca45831e1f01f570a47
hanwen <1106002609@qq.com>
2016-03-19 11:37:13 +0800

    出现次数最多的整数
    简单方法实现的

---
+++ src/com/test/MaxNumber2.java
@@ -0,0 +1,44 @@
+package com.test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaxNumber2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int[] a = new int[n];
+		for(int i = 0; i < n; i++)
+		{
+			a[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(a);
+		int loc = 0; 
+		int maxlen = 1, len = 1;
+		for(int i = 1; i < n; i++)
+		{
+			if(a[i] == a[i-1])
+			{
+				len++;
+			}
+			else if(len > maxlen)
+			{
+				maxlen = len;
+				len = 1;
+				loc = i - 1;
+			}
+			else
+				len = 1;
+		}
+		System.out.println(a[loc]);
+		
+		
+		
+		
+		
+		
+	}
+}
